e4cb4dfb79159f5c246a13fa89c22260e4f2d8d9
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2016-04-28 14:50:38 +0200

    Better Cython run-file. Fixes #11.

--- cython/run_fourier.py
+++ cython/run_fourier.py
@@ -12,6 +12,8 @@
 ###############################################################################
 # General
 from __future__ import print_function, with_statement, division
+import numpy as np
+import matplotlib as mpl  # Pyplot is loaded after setup!
 
 # Cython module
 import fourier
@@ -20,7 +22,6 @@ import fourier
 ###############################################################################
 # Set-up
 ###############################################################################
-# Flags
 compare = True
 
 # Initial setup
@@ -37,6 +38,30 @@ low = 1900.0
 high = 4100.0
 rate = 0.1
 
+
+###############################################################################
+# Prepare for Plotting
+###############################################################################
+def matplotlib_setup():
+    fig_width_pt = 328
+    inches_per_pt = 1.0 / 72.27
+    golden_mean = (np.sqrt(5.0) - 1) / 2.0
+    fig_width = fig_width_pt * inches_per_pt
+    fig_height = fig_width * golden_mean
+    fig_size = [fig_width, fig_height]
+    mpl.rc('text', usetex=True)
+    mpl.rc('figure', figsize=fig_size)
+    mpl.rc('font', size=8, family='serif')
+    mpl.rc('axes', labelsize=8)
+    mpl.rc('legend', fontsize=8)
+    mpl.rc('xtick', labelsize=8)
+    mpl.rc('ytick', labelsize=8)
+    mpl.rc('text.latex',
+           preamble=r'\usepackage[T1]{fontenc}\usepackage{libertine}\usepackage[libertine]{newtxmath}')
+
+matplotlib_setup()
+import matplotlib.pyplot as plt
+
 ###############################################################################
 # Script
 ###############################################################################
@@ -45,9 +70,7 @@ freq, powers = fourier.calc(datdir + infile, low, high, rate, unit, prep)
 
 # Compare to the true oscillations?
 if compare:
-    # Load modules
-    import numpy as np
-    import matplotlib.pyplot as plt
+    print('Plotting....')
 
     # Load correct oscillations
     oscfile = 'oscillations.dat'
@@ -55,9 +78,12 @@ if compare:
 
     # Plot
     plt.figure()
-    plt.plot(freq, powers, 'r-')
-    plt.plot(nu, A**2, 'g*')
+    plt.plot(freq, powers, 'r-', label=r'Calculated')
+    plt.plot(nu, A**2, 'g.', label=r'True')
     plt.title('Power spectrum')
-    plt.xlabel('nu [muHz]')
-    plt.ylabel('|V(t)|^2')
-    plt.savefig(outdir + 'test2.pdf')
+    plt.xlabel(r'$\nu$ [$\mu\textup{Hz}$]')
+    plt.ylabel(r'Power')
+    plt.legend()
+    plt.savefig(outdir + 'test2.pdf', bbox_inches='tight')
+
+    print('Done!')
