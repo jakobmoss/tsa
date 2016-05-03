031ad3c2a16a35cbeb1fb772ad05924186c8b579
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2016-05-03 14:01:09 +0200

    Cleaned-up arraylib

--- source/arrlib.c
+++ source/arrlib.c
@@ -6,14 +6,6 @@
 
 /* ~~~~~ Initialisations ~~~~~ */
 
-// Fill an array with zeros
-void arr_init_zero(double x[], size_t N)
-{
-    for (int i = 0; i < N; ++i) {
-        x[i] = 0;
-    }
-}
-
 // Fill an array with numbers: 0, ..., N-1 ; (C-style range)
 void arr_init_crange(double x[], size_t N)
 {
@@ -30,8 +22,8 @@ void arr_init_frange(double x[], size_t N)
     }
 }
 
-// Fill array x with N points of increment rate
-//  - NOTE: Use utility function to calculate N
+// Fill array x with N points of increment rate starting from a
+//  - NOTE: Uses utility function to calculate N
 void arr_init_linspace(double x[], double a, double rate, size_t N) 
 {
     for (int i = 0; i < N; ++i) {
@@ -42,26 +34,6 @@ void arr_init_linspace(double x[], double a, double rate, size_t N)
 
 
 /* ~~~~~ Return value for single array ~~~~~ */
-
-// Sum the elements of an array
-double arr_sum(double x[], size_t N)
-{
-    double sum = 0.0;
-    for (int i = 0; i < N; ++i) {
-        sum += x[i];
-    }
-    return sum;
-}
-
-// Sum the squared elements of an array
-double arr_sumsq(double x[], size_t N)
-{
-    double sum = 0.0;
-    for (int i = 0; i < N; ++i) {
-        sum += x[i]*x[i];
-    }
-    return sum;
-}
 
 // Average (mean) of elements in array
 double arr_mean(double x[], size_t N)
@@ -117,30 +89,6 @@ double arr_median(double x[], size_t N)
 
 /* ~~~~~ Array operations on single array ~~~~~ */
 
-// Take the cos of elements x and store in y.
-void arr_cos(double x[], double y[], size_t N)
-{
-    for (int i = 0; i < N; ++i) {
-        y[i] = cos(x[i]);
-    }
-}
-
-// Take the sin of elements x and store in y.
-void arr_sin(double x[], double y[], size_t N)
-{
-    for (int i = 0; i < N; ++i) {
-        y[i] = sin(x[i]);
-    }
-}
-
-// Multiply array x by scalar a and store in y
-void arr_scale(double x[], double a, double y[], size_t N)
-{
-    for (int i = 0; i < N; ++i) {
-        y[i] = x[i] * a;
-    }
-}
-
 // Add scalar a to array x -- IN-PLACE
 void arr_sca_add(double x[], double a, size_t N)
 {
@@ -155,36 +103,6 @@ void arr_diff(double x[], double y[], size_t N)
 {
     for (int i = 0; i < N-1; ++i) {
         y[i] = x[i+1] - x[i];
-    }
-}
-
-
-
-/* ~~~~~ Array operations on two arrays ~~~~~ */
-
-// Multiply arrays x, y and store in z
-void arr_mult(double x[], double y[], double z[], size_t N)
-{
-    for (int i = 0; i < N; ++i) {
-        z[i] = x[i] * y[i];
-    }
-}
-
-
-// Add arrays x, y and store in z
-void arr_add(double x[], double y[], double z[], size_t N)
-{
-    for (int i = 0; i < N; ++i) {
-        z[i] = x[i] + y[i];
-    }
-}
-
-
-// Add the square of the arrays x, y and store in z
-void arr_addsq(double x[], double y[], double z[], size_t N)
-{
-    for (int i = 0; i < N; ++i) {
-        z[i] = x[i]*x[i] + y[i]*y[i];
     }
 }
 
--- source/arrlib.h
+++ source/arrlib.h
@@ -1,5 +1,3 @@
-void arr_init_zero(double x[], size_t N);
-
 void arr_init_crange(double x[], size_t N);
 
 void arr_init_frange(double x[], size_t N);
@@ -7,32 +5,15 @@ void arr_init_frange(double x[], size_t N);
 void arr_init_linspace(double x[], double a, double rate, size_t N);
 
 
-double arr_sum(double x[], size_t N);
-
-double arr_sumsq(double x[], size_t N);
-
 double arr_mean(double x[], size_t N);
 
 double arr_median(double x[], size_t N);
 
 
-void arr_cos(double x[], double y[], size_t N);
-
-void arr_sin(double x[], double y[], size_t N);
-
-void arr_scale(double x[], double a, double y[], size_t N);
-
 void arr_sca_add(double x[], double a, size_t N);
 
 void arr_diff(double x[], double y[], size_t N);
 
-
-
-void arr_mult(double x[], double y[], double z[], size_t N);
-
-void arr_add(double x[], double y[], double z[], size_t N);
-
-void arr_addsq(double x[], double y[], double z[], size_t N);
 
 size_t arr_util_getstep(double a, double b, double rate);
 
